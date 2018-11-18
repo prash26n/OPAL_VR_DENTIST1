a703250462df46b5e7979ce6d5c4863efb7a392d
Khanna <rishabhkhanna@ufl.edu>
2018-11-18 13:53:26 -0500

    script to active anxiety level slider on collider

---
+++ Assets/Resources/corridor/scenes/scripts/DisplaySlider.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplaySlider : MonoBehaviour {
+
+    // Use this for initialization
+    showTrigger triggerObject;
+
+    void Start () {
+         triggerObject = GameObject.Find("Trigger").GetComponent<showTrigger>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnTriggerEnter(Collider other)
+    {   
+        
+        
+        triggerObject.show_trigger();
+    }
+
+
+
+}
---
+++ Assets/Resources/corridor/scenes/scripts/showTrigger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class showTrigger : MonoBehaviour {
+
+    // Use this for initialization
+    public Renderer rend;
+    void Start () {
+        //StartCoroutine(hide());
+        //rend = GetComponent<Renderer>();
+        //rend.enabled = false;
+        Debug.Log("trigger script  START called");
+        this.gameObject.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void show_trigger()
+    {
+        Debug.Log("trigger script called");
+        //StartCoroutine(ShowAndHide()); // 1 second
+       // rend.enabled = true;
+        this.gameObject.SetActive(true);
+
+
+    }
+
+}
