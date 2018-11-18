57426f8e2cad102b2915145f98ad5c9e65f9371c
Khanna <rishabhkhanna@ufl.edu>
2018-11-18 14:38:47 -0500

    reception intro disappear after 10 seconds

---
+++ Assets/Resources/corridor/scenes/scripts/disappearAfterSeconds.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class disappearAfterSeconds : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(Hide(10.0f));
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    IEnumerator Hide(float delay)
+    {
+        //this.gameObject.SetActive(true);
+        yield return new WaitForSeconds(delay);
+        this.gameObject.SetActive(false);
+    }
+}
