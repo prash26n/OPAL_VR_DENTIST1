b2445c655d5f0c3198b11703846f73c739b12d56
Khanna <rishabhkhanna@ufl.edu>
2018-12-01 16:58:44 -0500

    slider appearing on both the doors

---
+++ Assets/Scripts/like_display_slider.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class like_display_slider : MonoBehaviour {
+
+    // Use this for initialization
+    showTrigger triggerObject;
+
+    void Start()
+    {
+        triggerObject = GameObject.Find("Trigger").GetComponent<showTrigger>();
+        triggerObject.hide();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+
+
+        triggerObject.show_trigger();
+    }
+
+}
